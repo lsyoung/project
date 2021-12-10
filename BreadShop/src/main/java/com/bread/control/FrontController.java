100c27771d3f7d02fa709082bf8ed57cd4f76d77
lsyoung <sulyoung7@gmail.com>
2021-12-10 13:52:43 +0900

    test11

--- BreadShop/src/main/java/com/bread/control/FrontController.java
+++ BreadShop/src/main/java/com/bread/control/FrontController.java
@@ -12,15 +12,19 @@ import javax.servlet.http.HttpServletResponse;
 public class FrontController extends HttpServlet {
 
 	HashMap<String, Controller> list = null;
-	
+
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		list = new HashMap<String, Controller>();
 	}
-	
+
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) 
-			throws ServletException, IOException {
-	
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String url = req.getRequestURI();
+		String context = req.getContextPath();
+		String path = url.substring(context.length());
+
+		Controller subCont = list.get(path);
+		subCont.execute(req, res);
 	}
 }
